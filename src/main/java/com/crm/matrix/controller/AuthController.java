f3c392a9d436ac4d22bfff20c58dbc089d3e3b87
Sandeep Konda <kondasandeep56@gmail.com>
2026-08-31 18:45:25 +0530

    update cicd

--- src/main/java/com/crm/matrix/controller/AuthController.java
+++ src/main/java/com/crm/matrix/controller/AuthController.java
@@ -1,4 +1,21 @@
 package com.crm.matrix.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
 public class AuthController {
+
+    @GetMapping
+    public String getUsers() {
+        return "Users working";
+    }
+
+    @GetMapping("/{id}")
+    public String getUser(@PathVariable Long id) {
+        return "User " + id;
+    }
 }
