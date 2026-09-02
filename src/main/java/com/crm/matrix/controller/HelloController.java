7ff44d9d5355a452ea27a2f9e4205c2af93c7aa0
Sandeep Konda <kondasandeep56@gmail.com>
2026-09-02 15:25:35 +0530

    update

---
+++ src/main/java/com/crm/matrix/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.crm.matrix.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hi Ra ";
+    }
+}
